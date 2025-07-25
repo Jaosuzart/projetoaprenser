2a199ba37a8a641216f7f6059e927c60d47d1f90
Jaosuzart <joaomarcelosuzartcastro@gmail.com>
2025-07-25 18:15:46 -0300

    Enviando atualizacao ao github

--- script/script.js
+++ script/script.js
@@ -1,43 +1,55 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const menuToggle = document.querySelector(".menu-toggle");
+  const menuToggle = document.getElementById("hamburguer-button");
   const menu = document.getElementById("menu");
-  const menuClose = document.querySelector(".menu-close");
-  const overlay = document.createElement("div");
-
-  overlay.classList.add("menu-overlay");
-  document.body.appendChild(overlay);
-
-  function toggleMenu() {
-    menuToggle.classList.toggle("active");
-    menu.setAttribute(
-      "aria-hidden",
-      menu.getAttribute("aria-hidden") === "true" ? "false" : "true"
-    );
-    document.body.style.overflow =
-      menu.getAttribute("aria-hidden") === "true" ? "" : "hidden";
-    overlay.style.display =
-      menu.getAttribute("aria-hidden") === "true" ? "none" : "block";
+  const navLinks = document.querySelectorAll(".header_nav a");
+  const menuCloseBtn = document.querySelector(".menu-close-btn");
+  function openMenu() {
+    menu.setAttribute("aria-hidden", "false");
+    menuToggle.setAttribute("aria-expanded", "true");
   }
 
-  menuToggle.addEventListener("click", toggleMenu);
-  menuClose.addEventListener("click", toggleMenu);
-  overlay.addEventListener("click", toggleMenu);
+  function closeMenu() {
+    menu.setAttribute("aria-hidden", "true");
+    menuToggle.setAttribute("aria-expanded", "false");
+  }
 
-  // Fecha o menu ao clicar em links internos
-  const menuLinks = menu.querySelectorAll('a[href^="#"]');
-  menuLinks.forEach((link) => {
-    link.addEventListener("click", toggleMenu);
+  menuToggle.addEventListener("click", openMenu);
+
+  menuCloseBtn.addEventListener("click", closeMenu);
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      if (window.innerWidth <= 768) {
+        closeMenu();
+      }
+      if (this.getAttribute("href").startsWith("#")) {
+        event.preventDefault();
+        const targetId = this.getAttribute("href").substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+          const headerOffset = document.querySelector(
+            ".wrapperContainer-fixed"
+          ).offsetHeight;
+          const elementPosition = targetElement.getBoundingClientRect().top;
+          const offsetPosition =
+            elementPosition + window.pageYOffset - headerOffset;
+
+          window.scrollTo({
+            top: offsetPosition,
+            behavior: "smooth",
+          });
+        }
+      }
+    });
   });
 
-  // Ajusta o menu em redimensionamento da tela
+  // Evento para redimensionar a janela
   window.addEventListener("resize", function () {
     if (window.innerWidth > 768) {
       menu.setAttribute("aria-hidden", "false");
-      menuToggle.classList.remove("active");
-      document.body.style.overflow = "";
-      overlay.style.display = "none";
     } else {
-      menu.setAttribute("aria-hidden", "true");
+      closeMenu();
     }
   });
 });
